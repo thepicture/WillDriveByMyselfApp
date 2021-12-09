3f8cb282693b97ca68088c7e4fb3ea11f64ab902
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-09 14:17:39 +0000

    Designed AdminModeCheckingView

--- ViewModels/AdminModeCheckingViewModel.cs
+++ ViewModels/AdminModeCheckingViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace WillDriveByMyselfApp.ViewModels
+﻿using System.Windows.Input;
+using WillDriveByMyselfApp.Commands;
+
+namespace WillDriveByMyselfApp.ViewModels
 {
     public class AdminModeCheckingViewModel : ViewModelBase
     {
+
+        private string _code;
+
+        public string Code
+        {
+            get => _code; set
+            {
+                _code = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private RelayCommand activateAdminModeCommand;
+
+        public ICommand ActivateAdminModeCommand
+        {
+            get
+            {
+                if (activateAdminModeCommand == null)
+                {
+                    activateAdminModeCommand = new RelayCommand(ActivateAdminMode);
+                }
+
+                return activateAdminModeCommand;
+            }
+        }
+
+        private void ActivateAdminMode(object commandParameter)
+        {
+        }
     }
 }
---
+++ Views/AdminModeCheckingView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WillDriveByMyselfApp.Views
+{
+    /// <summary>
+    /// Interaction logic for AdminModeCheckingView.xaml
+    /// </summary>
+    public partial class AdminModeCheckingView : UserControl
+    {
+        public AdminModeCheckingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
